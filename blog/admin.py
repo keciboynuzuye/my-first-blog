bdba4c3ba5b57a6a7199e1fce1101a2ad555df76
Your Name <you@example.com>
2016-02-13 15:23:21 +0200

    post icin draft ve delete eklendi

--- blog/admin.py
+++ blog/admin.py
@@ -1,4 +1,11 @@
 from django.contrib import admin
 from .models import Post
+from django.contrib.auth.models import User
 
-admin.site.register(Post)
+class PostAdmin(admin.ModelAdmin):
+    list_display = ('title', 'published_date')
+    search_fields = ['title']
+    #exclude = ('created_date',)
+    readonly_fields = ('created_date',)
+#admin.site.register(User)
+admin.site.register(Post,PostAdmin)
